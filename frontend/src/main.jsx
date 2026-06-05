dd04f5ce6621090a3c87622302612d0158c3fa6c
Sergio Sorgente <sorgentesergio@gmail.com>
2026-06-05 16:08:55 +0200

    imported bootstrap

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css'//this is where css style is implemented
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
