7dc82227ea4625bc32a98e20d620f4a2c0eb4a67
Sergio Sorgente <sorgentesergio@gmail.com>
2026-06-05 16:31:05 +0200

    Import bootstrap before css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.css';// i added this for bootstrap grid style
+// Put any other imports below so that CSS from your components takes precedence over default styles.
 import './index.css'//this is where css style is implemented
 import App from './App.jsx'
 
