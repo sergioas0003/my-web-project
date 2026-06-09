db358b60b7c61c7a0ef7786856da232f696834ed
Sergio Sorgente <sorgentesergio@gmail.com>
2026-06-09 14:21:30 +0200

    button adding

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="container mt-5">//this keeps the components centred
+      <h1>Hello React</h1>
+
+      <button className="btn btn-dark me-2">//makes the button black
+        Click Me 
+      </button>
+
+      <button className="btn btn-primary">//blue button with spacing
+        Click Me2
+      </button>
+    </div>
+  );
+}
+
+export default App;
