6ee3a0c4a18af287c6ea9465c369b9b8dac46a1b
Sergio Sorgente <sorgentesergio@gmail.com>
2026-06-16 09:56:31 +0200

    inserting an input text field

--- frontend/src/Menu.jsx
+++ frontend/src/Menu.jsx
@@ -4,7 +4,10 @@ function Menu() {
       <h2>Lunch Menu</h2>
       <p>Chicken Wrap 10.</p>
       <p>Pizza 20.</p>
+       <input type="text" 
+       className="form-control"/>
     </div>
+
   );
 }
 
