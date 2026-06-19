5ff7167f5d15eed479c5c114276ecc936daec839
Sergio Sorgente <sorgentesergio@gmail.com>
2026-06-19 11:41:44 +0200

    Entity for MenuItems table made

---
+++ backend/src/main/java/com/restaurant/backend/MenuItems.java
@@ -0,0 +1,55 @@
+import jakarta.persistence.*;// we need this import for @Entity, @Table and more
+
+@Entity//tells spring that this is a database
+@Table(name = "menu_items")
+public class MenuItem {//this maps the table to java
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private int id;// encapsulation requires variables to be made private 
+    private String name;
+    private String description;
+    private double price;
+
+    public MenuItem() {// We need to make an empty constructor 
+    }
+
+    public MenuItem(String n, String d, double p) {
+        name = n;
+        description = d;
+        price = p;
+    }
+
+    public int getId() {//each variable requires a getter and setter method due to encapsulation
+        return id;
+    }
+
+    public void setId(int i) {
+        id = i;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String d) {
+        description = d;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double p) {
+        price = p;
+    }
+}
