38126d03f3a72a999900444eef09d62ca66316d1
Sergio Sorgente <sorgentesergio@gmail.com>
2026-06-22 13:49:04 +0200

    fetch menu data to menu page

--- frontend/src/Menu.jsx
+++ frontend/src/Menu.jsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from "react";
+
 function Menu() {
+  const [menuItems, setMenuItems] = useState([]);
+
+    useEffect(() => {
+
+        fetch("http://localhost:8080/api/menu")
+            .then(response => response.json())
+            .then(data => setMenuItems(data));}, []);
+  
   return (
     <div>
       <h2>Lunch Menu</h2>
-      <p>Chicken Wrap 10.</p>
-      <p>Pizza 20.</p>
+      <p>Items loaded: {menuItems.length}</p>
+      <div>
+       {menuItems.map(item => (
+                <div key={item.id}>
+                    <h3>{item.name}</h3>
+                    <p>{item.description}</p>
+                    <p>€{item.price}</p>
+                </div>
+            ))}
+            </div>
        <input type="text" 
        className="form-control"/>
     </div>
 
   );
 }
+
 
 export default Menu;
