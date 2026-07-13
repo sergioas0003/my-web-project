1ed3b681b3b7d4606b4ccdbe4fee3d55734cb4d1
Sergio Sorgente <sorgentesergio@gmail.com>
2026-07-13 15:18:42 +0200

    Check reservation auto update fixed.

--- frontend/src/CheckReservation.jsx
+++ frontend/src/CheckReservation.jsx
@@ -7,11 +7,32 @@ function CheckReservation() {
   const [searched, setSearched] = useState(false);
   
   useEffect(() => {
+        loadReservations();
+      }, []);
 
-        fetch("http://localhost:8080/api/reservations") // retrieves data from menu
-            .then(response => response.json())
-            .then(data => setReservation(data));}, 
-    []);
+    async function DeleteItem(id, email) {
+
+    await fetch("http://localhost:8080/api/reservations/" + id, {
+        method: "DELETE"
+    });
+
+    const updatedReservations = await loadReservations();
+
+    const results = updatedReservations.filter(r =>
+        r.email.toLowerCase() === email.toLowerCase()
+    );
+
+    setMatches(results);
+}
+  
+  async function loadReservations(){
+    const response = await fetch("http://localhost:8080/api/reservations");
+    const data = await response.json();
+
+    setReservation(data);
+
+    return data;
+  }  
 
   function Search(email) {
     const results = reservation.filter(r => r.email.toLowerCase() === email.toLowerCase());
@@ -35,10 +56,16 @@ function CheckReservation() {
           onClick={() => Search(email)}>
             Search
         </button>
-        
+        <hr />
         {matches.map(r => (
           <div key={r.id}>
             <p>Reservation Name: {r.name}</p>
+            <span>
+                  <button className="button"
+                  onClick={() => DeleteItem(r.id, r.email)}>
+                    Cancel Reservation
+                  </button>
+                </span>
             <p>Email: {r.email}</p>
             <p>Date: {r.date}</p>
             <p>Time: {r.time} pm</p>
